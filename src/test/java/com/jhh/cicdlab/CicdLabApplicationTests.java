f0f4898005ab5c4890d143e62f873f432354cfa6
Jaehwan Hyun <jec0101@naver.com>
2024-09-16 20:25:01 +0900

    Initial Commit

---
+++ src/test/java/com/jhh/cicdlab/CicdLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhh.cicdlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
